080b61ef85c0a1b4f454e52cb6b22fee61284320
Daniel Elias <danieliasantos.dev@protonmail.com>
2018-06-13 09:20:26 -0300

    Refactor do relatorio e inicio questao um

---
+++ Questoes/Questao01.m
@@ -0,0 +1,38 @@
+## Copyright (C) 2018 Daniel Elias
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} Questao01 (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Daniel Elias <daniel@danielias-note>
+## Created: 2018-06-13
+
+#{
+
+Para f (x) = 3 x − 1, definida no intervalo x ∈ (1, +∞), sejam x 0 = 1, 5, x 1 = 1, 6 e x 2 =
+1, 9. Construa polinômios interpoladores de grau um (P 1 (x), interpolação linear), grau dois (P 2 (x),
+interpolação quadrática) e grau 3 (P 3 (x), interpolação cúbica) para determinar uma aproximação de
+f (1, 7) e encontre o erro absoluto para cada aproximação no ponto 1, 7. Plote as funções f (x), P 1 (x), P 2 (x)
+e P 3 (x) em um mesmo gráfico, inserindo nome nos eixos, tı́tulo e legenda. Determine as equações dos
+polinômios P 1 (x), P 2 (x) e P 3 (x). Faça uma interpretação dos resultados.
+
+#}
+
+function [retval] = Questao01 (input1, input2)
+
+endfunction
